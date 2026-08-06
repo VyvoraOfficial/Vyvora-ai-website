5e0261804e7e29875ebc844cb85698e893b2dff9
Ilesh Singh <vyvora.ai.business@gmail.com>
2026-08-06 15:54:17 +0530

    Add animated counters

--- script.js
+++ script.js
@@ -20,4 +20,32 @@ window.addEventListener("scroll", () => {
         navbar.style.background = "rgba(8,17,31,.65)";
     }
 
+});const counters = document.querySelectorAll(".counter");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            const counter = entry.target;
+            const target = +counter.dataset.target;
+
+            let count = 0;
+
+            const update = () => {
+                count += Math.ceil(target / 50);
+
+                if (count >= target) {
+                    counter.innerText = target + "+";
+                } else {
+                    counter.innerText = count;
+                    requestAnimationFrame(update);
+                }
+            };
+
+            update();
+            observer.unobserve(counter);
+        }
+    });
 });
+
+counters.forEach(counter => observer.observe(counter));
